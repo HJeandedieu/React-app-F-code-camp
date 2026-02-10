d4164125a15335fa32c3f585414b70645c333e78
jd <hashimweyesujeandedie@gmail.com>
2026-02-10 22:03:22 +0200

    Added MoodBoard.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { MoodBoard } from "./components/MoodBoard";
+
+function App(){
+  return(
+    <div>
+    <MoodBoard />
+    </div>
+  )
+}
+export default App;
---
+++ src/moodBoard.jsx
@@ -0,0 +1,36 @@
+export const MoodBoardItem = ({color, image,description})=>{
+  return (
+    <div className="mood-board-item" 
+    styles={{backgroundColor:color}}>
+    <img className="mood-board-image"
+    src={image} />
+    <h3 className = "mood-board-text"> {description}</h3>
+    </div>
+  )
+}
+
+export function MoodBoard (){
+  return (
+    <div className="mood-board">
+    <h1 className="mood-board-heading">Destination Mood Board</h1>
+    <MoodBoardItem 
+    color="white"
+    image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg"
+    description="Warm-temperature and sunny morning"
+    />
+    
+    <MoodBoardItem 
+    color="Yellow"
+    image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg"
+    description="Breezy Afternoon with cloudy sky."
+    />
+
+    <MoodBoardItem 
+    color="Orange"
+    image="https://cdn.freecodecamp.org/curriculum/labs/grass.jpg"
+    description="Winter is comming"
+    />
+
+    </div>
+  )
+}
